86acb130d43519f438c016808d4718f861c42e2c
afzaanhakim <afzaanhakim19@gmail.com>
2021-12-21 12:26:31 -0500

    added wallet connect functionality, checking if wallet is injected and just basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import React, { useEffect, useState} from 'react';
+
+import { useWeb3 } from '@3rdweb/hooks';
+
 const App = () => {
+
+  const { connectWallet, address, error, provider} = useWeb3();
+
+  console.log("address:", address, "provider:", provider, "error:",  error)
+  
+  if (!address) {
+    return (
+      <div className="landing">
+      <h1> 🍕 🍱 RecipeDAO 🍱 🍕 </h1>
+      <button onClick={() => connectWallet("injected")} className="btn-hero"> Click Me To Connect Wallet</button>
+    </div>
+    )
+  }
+  
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      
+      <h3>✅ ✅ You are Connected to RecipeDao with address:</h3>
+      <a> {address}</a>
     </div>
   );
 };
