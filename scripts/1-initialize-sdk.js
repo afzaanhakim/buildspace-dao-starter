924c44d4a55db123bc8e4410394280279ef43746
afzaanhakim <afzaanhakim19@gmail.com>
2021-12-21 13:27:20 -0500

    initializing thirdweb and checking if env variables are configured ok

---
+++ scripts/1-initialize-sdk.js
@@ -0,0 +1,39 @@
+import { ThirdwebSDK } from "@3rdweb/sdk";
+import ethers from "ethers";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+
+//basic checks to see if dotenv working as expected
+if (!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY == "") {
+  console.log("🛑 Private key not found.");
+}
+
+if (!process.env.ALCHEMY_API_URL || process.env.ALCHEMY_API_URL == "") {
+  console.log("🛑 Alchemy API URL not found.");
+}
+
+if (!process.env.WALLET_ADDRESS || process.env.WALLET_ADDRESS == "") {
+  console.log("🛑 Wallet Address not found.");
+}
+
+const sdk = new ThirdwebSDK(
+  new ethers.Wallet(
+    process.env.PRIVATE_KEY,
+    ethers.getDefaultProvider(process.env.ALCHEMY_API_URL)
+  )
+);
+
+(async () => {
+  try {
+    const apps = await sdk.getApps();
+    console.log("✅ your app adress is", apps[0].address)
+  }
+  catch (err) {
+    console.log(" ❌ failed to get due to error", err);
+    process.exit(1)
+  }
+}) ()
+
+export default sdk;
